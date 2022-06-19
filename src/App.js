4db76b2a6f3ea2fdf65880a74999de65e8d6f3f5
Suluv <lovish.vaish@live.com>
2022-06-19 12:53:59 -0700

    suluv updating text

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React from pragim
+          Cricket Tournament Project
         </a>
       </header>
     </div>
